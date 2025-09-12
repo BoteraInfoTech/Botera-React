6cce6e04d86520f9fc5acdece43119191e20b8e2
Akil Saiyad <akil@socialpilot.co>
2025-09-13 01:52:30 +0530

    Add PostHog session recording script to index.html and update authentication logic in auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
 export const isAuthenticated = () => {
-  return true;
-  // return localStorage.getItem("userToken") !== null;
+  // return true;
+  return localStorage.getItem("userToken") !== null;
 };
