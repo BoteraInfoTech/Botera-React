153ad9ea3474ce4c4a46d590a2d0b4209f70ca1e
Akil Saiyad <akil@socialpilot.co>
2025-09-24 23:36:12 +0530

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,3 @@
-export const BACKEND_POINT =
-  // "https://botera-main.onrender.com";
+export const BACKEND_POINT = "https://botera-main.onrender.com";
 
-  "http://localhost:8000";
+// "http://localhost:8000";
